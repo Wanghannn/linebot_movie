2295568a9cb8b5b250bab86b3546044afddd6dbc
WangHannn <ivory880317@gmail.com>
2022-05-18 14:18:48 +0800

    CommitTest

---
+++ app.py
@@ -0,0 +1,92 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+
+
+#======這裡是呼叫的檔案內容=====
+from bot_template import *
+from bigdata import *
+#======這裡是呼叫的檔案內容=====
+
+#======python的函數庫==========
+import tempfile, os
+import datetime
+import time
+#======python的函數庫==========
+
+app = Flask(__name__)
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+# Channel Access Token
+line_bot_api = LineBotApi('PYmA6HohelmsYT68pbad1kY4gO1DkzVVih9o4/q/kPS7LEW5C2LryB3dtGq5PgdsZACzgx9Bh1ah+Cf5xKIN0n4LKBeNOfRUDsOFEBQbt4H1XrG1JHq4Nh6FbYbe+qXsyUUQXwGa6cVqCtQh4aJFJgdB04t89/1O/w1cDnyilFU=')
+# Channel Secret
+handler = WebhookHandler('110f9e33ec37530666ae272feca3aff7')
+
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+
+# 處理訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    msg = event.message.text
+    if 'imagemap_message' in msg:
+        message = imagemap_message()
+        line_bot_api.reply_message(event.reply_token, message)
+    elif 'buttons_message' in msg:
+        message = buttons_message()
+        line_bot_api.reply_message(event.reply_token, message)
+    elif 'Confirm_Template' in msg:
+        message = Confirm_Template()
+        line_bot_api.reply_message(event.reply_token, message)
+    elif 'Carousel_Template' in msg:
+        message = Carousel_Template()
+        line_bot_api.reply_message(event.reply_token, message)
+    elif 'picture' in msg:
+        message = picture()
+        line_bot_api.reply_message(event.reply_token, message)
+    # 我們自己的功能
+    elif '電影' == msg:
+        message = choose_funtion()
+        line_bot_api.reply_message(event.reply_token, message)
+    else:
+        message = TextSendMessage(text=msg)
+        line_bot_api.reply_message(event.reply_token, message)
+
+@handler.add(PostbackEvent)
+def handle_message(event):
+    print(event.postback.data)
+
+
+@handler.add(MemberJoinedEvent)
+def welcome(event):
+    uid = event.joined.members[0].user_id
+    gid = event.source.group_id
+    profile = line_bot_api.get_group_member_profile(gid, uid)
+    name = profile.display_name
+    message = TextSendMessage(text=f'{name}歡迎加入')
+    line_bot_api.reply_message(event.reply_token, message)
+        
+        
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
---
+++ bigdata.py
@@ -0,0 +1,34 @@
+#這些是LINE官方開放的套件組合透過import來套用這個檔案上
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import *
+
+# 我們自己的程式
+def choose_funtion():
+    message = TemplateSendMessage(
+        alt_text='第一步驟！！',
+        template=ButtonsTemplate(
+            thumbnail_image_url="https://pic2.zhimg.com/v2-de4b8114e8408d5265503c8b41f59f85_b.jpg",
+            title="今天想查詢什麼？",
+            text="選擇今天要查詢的項目",
+            actions=[
+                MessageTemplateAction(
+                    label="1. 查詢本週新片",
+                    text="我要看本週新片"
+                ),
+                MessageTemplateAction(
+                    label="2. 查詢排行榜",
+                    text="我要看排行榜"
+                ),
+                MessageTemplateAction(
+                    label="3. 我要選影廳",
+                    text="有哪些影廳可以選？"
+                ),
+                MessageTemplateAction(
+                    label="4. 我要看電影",
+                    text="有什麼電影可以看呢？"
+                )
+            ]
+        )
+    )
+    return message
